6b075798129f06e10ec47a6de1ab288b0ca5f084
RGJC33 <ju33cicuto@gmail.com>
2023-11-12 14:09:42 -0300

    atualizado

---
+++ src/scripts/database.js
@@ -0,0 +1,98 @@
+export const products = [
+  {
+    id: 1,
+    name: "Pousada",
+    image: 'src/assets/modelos-de-pousadas-1.webp',
+    price: 600,
+  },
+  {
+    id: 2,
+    name: "Pousada",
+    image: "src/assets/modelos-de-pousadas-1.webp",
+    price: 540,
+  },
+  {
+    id: 3,
+    name: "Pousada",
+    image: "src/assets/modelos-de-pousadas-1.webp",
+    price: 783,
+  },
+  {
+    id: 4,
+    name: "Pousada",
+    image: "src/assets/modelos-de-pousadas-1.webp",
+    price: 500,
+  },
+  {
+    id: 5,
+    name: "Pousada",
+    image: "src/assets/modelos-de-pousadas-1.webp",
+    price: 990,
+  },
+  {
+    id: 6,
+    name: "Pousada",
+    image: "src/assets/modelos-de-pousadas-1.webp",
+    price: 800,
+  },
+  {
+    id: 7,
+    name: "Pousada",
+    image: "src/assets/modelos-de-pousadas-1.webp",
+    price: 890,
+  },
+  {
+    id: 8,
+    name: "Pousada",
+    image: "src/assets/modelos-de-pousadas-1.webp",
+    price: 758,
+  },
+  {
+    id: 9,
+    name: "Pousada",
+    image: "src/assets/modelos-de-pousadas-1.webp",
+    price: 200,
+  },
+  {
+    id: 10,
+    name: "Pousada",
+    image: "src/assets/modelos-de-pousadas-1.webp",
+    price: 250,
+  },
+  {
+    id: 11,
+    name: "Pousada",
+    image: "src/assets/modelos-de-pousadas-1.webp",
+    price: 140,
+  },
+  {
+    id: 12,
+    name: "Pousada",
+    image: "src/assets/modelos-de-pousadas-1.webp",
+    price: 190,
+  },
+  {
+    id: 13,
+    name: "Pousada",
+    image: "src/assets/modelos-de-pousadas-1.webp",
+    price: 300,
+  },
+  {
+    id: 14,
+    name: "Pousada",
+    image: "src/assets/modelos-de-pousadas-1.webp",
+    price: 289,
+  },
+  {
+    id: 15,
+    name: "Pousada",
+    image: "src/assets/modelos-de-pousadas-1.webp",
+    price: 320,
+  },
+  {
+    id: 16,
+    name: "Pousada",
+    image: "src/assets/modelos-de-pousadas-1.webp",
+    price: 350,
+  },
+];
